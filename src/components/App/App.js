6c62df3bcbf6079a64433e1e8c0c296a33bef273
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-30 13:37:31 -0600

    Add beginning of Hamburger Menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import PropTypes from "prop-types";
 import Header from "../../containers/Header/index";
 import { fetchMovie } from "../../util/apiCalls.js";
+import { Route, Switch, Redirect, Link } from "react-router-dom";
 // import MainContainer from "../MainContainer/MainContainer.js";
 
 export default class App extends Component {
@@ -10,7 +11,7 @@ export default class App extends Component {
     super();
     this.state = {
       allFilms: [],
-      randomFilm: {},
+      randomFilm: {}
     };
   }
 
@@ -35,9 +36,43 @@ export default class App extends Component {
   render() {
     return (
       <section className="App mainBody">
-        <header className="headerContainer">
+        {/* <div className="App mainBody">
+          {/* <Switch>
+            {/* <Route exact path="/" component={} /> */}
+            {/* <Route path="/Header" component={Header} />
+            <Redirect to="/" /> */}
+          {/* </Switch> */}
+        {/* </div> */} 
+        <div>
           <Header />
-        </header>
+        </div>
+        <nav role="navigation">
+          <div id="menuToggle">
+            <input type="checkbox" />
+            <span />
+            <span />
+            <span />
+            <ul id="menu">
+              <h4 className="user-name" />
+              <Link to="/MovieContainer">
+                <li>Home</li>
+              </Link>
+              <hr />
+              <Link to="MovieContainer/favorites">
+                <li>Favorites</li>
+              </Link>
+              <hr />
+              <Link to="">
+                <li>About</li>
+              </Link>
+              <hr />
+              <Link to="/Login">
+                <li>Sign Out</li>
+              </Link>
+              <hr />
+            </ul>
+          </div>
+        </nav>
         <main className="mainCardArea">
           <p>{this.state.randomFilm.title}</p>
           <p>{this.state.randomFilm.description}</p>
@@ -46,5 +81,3 @@ export default class App extends Component {
     );
   }
 }
-
-
