c16f26af6212c89022dce081346637473fa7bea3
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-30 15:50:17 -0600

    Add loading message to peoplecontainer

--- src/containers/PeopleContainer/index.js
+++ src/containers/PeopleContainer/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { fetchPeople } from "../../util/apiCalls";
+import Card from '../../components/Card/index';
 
 export default class CardContainer extends Component {
   constructor() {
     super();
     this.state = {
-      characters: []
+      characters: [],
+      loading: false
     };
   }
 
@@ -13,15 +15,43 @@ export default class CardContainer extends Component {
     fetchPeople()
     .then(people =>
       this.setState({
-        characters: people
+        people,
+        loading: true
       })
     );
+    this.formatPerson();
   }
 
+  formatPerson=()=> {
+    let updatedPeople = this.state.characters.map(person => {
+      let { name, age, gender,  } = person;
+      return { name, age, gender };
+    });
+
+    this.setState({ 
+      people: updatedPeople, 
+      loading: false 
+    });
+    
+  }; 
+
   render() {
+    const peopleCards = this.state.characters.map(person => 
+      <Card 
+        key={person.id}
+        data={person}
+        category="person"
+      />
+    )
+
+    const loadingMessage = (
+      <div className="loadingMessage">
+        <p>Information loading. Please Hold...</p>
+      </div>
+    );
     return (
-      <section>
-      <p>Test</p>
+      <section className="cardContainer">
+        {this.state.loading ? loadingMessage : peopleCards}
       </section>
     );
   }
