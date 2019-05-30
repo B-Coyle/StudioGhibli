792a27f7e80e71fa575c8b92b182991830b36c2b
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-30 11:50:26 -0600

    Add header to app and create header file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import Header from '../../containers/Header/index';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state= {
+      films = [],
+      randomFilm = {}
+    }
+  }
+
+  componentDidMount() {
+    fetchMovie()
+    .then(films => this.setState({
+        films: films.results
+    }, () => this.assignRandomInformation()
+    ) )
+ }
+
+ assignRandomInformation= () => {
+  let randomNumber = Math.floor(Math.random() * this.state.allFilms.length )
+  this.setState ({
+  randomFilm: this.state.allFilms[randomNumber],
+  })
+}
+
+
   render() {
     return (
-      <div className="App">
-        <h1>Another Todo Box</h1>
-      </div>
+      <section className="App mainBody">
+        <header className="headerContainer">
+          <Header />
+        </header>
+        <main className="mainCardArea">
+          <p>{this.state.randomFilm.title}</p>
+          <p>{this.state.randomFilm.description}</p>
+        </main>
+      </section>
     );
   }
 }
