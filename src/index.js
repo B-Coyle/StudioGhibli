401e08e66d5dbb454468564421bf869272702115
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-30 11:33:01 -0600

    Update main index.js with provider and router imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import './index.scss';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={ store }>
+    <BrowserRouter>
+      < Route 
+        path='/'
+        component={ App }
+      />
+    </BrowserRouter>
+  </Provider>
+  ,document.getElementById('root')
 );
+
