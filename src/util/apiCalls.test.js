1fe263881acd914968bf90f6e8d861f0c5e796d9
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-31 17:34:17 -0600

    Add several passing tests for apiCalls fetch

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -1,124 +1,99 @@
-import * as actions from "./apiCalls";
+import {fetchMovie, fetchPeople, fetchLocations} from './apiCalls';
 
-describe("apiCalls fetch", () => {
-  describe("fetchMovie", () => {
-    it("calls fetch with the correct data when adding a new user", async () => {
+describe('API Calls', () => {
+  describe('fetchMovie', () => {
+    let mockMovieData;
+    beforeEach(() => {
+      mockMovieData = {
+        episode_id: 4,
+        title: "Spirited Away"
+      };
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
-          json: () => {
-            return Promise.resolve();
-          },
-          ok: true
+          ok: true,
+          json: () => Promise.resolve(mockMovieData)
         });
       });
-      const mockUser = {
-        name: "Bridgett",
-        email: "bac@gmail.com",
-        password: "Gus"
-      };
-      const expected1 = "http://localhost:3000/api/users/new";
-      const expected2 = {
-        method: "POST",
-        body: JSON.stringify(mockUser),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      };
-      await actions.addUser(mockUser);
-      expect(window.fetch).toHaveBeenCalledWith(expected1, expected2);
     });
 
-    it("should throw an error when the fetch fails", async () => {
-      const mockUser = {
-        name: "Bridgett",
-        email: "bac@gmail.com",
-        password: "Gus"
-      };
-      window.fetch = () => Promise.resolve({ ok: false });
-      const expected = Error();
-      expect(actions.addUser(mockUser)).rejects.toEqual(expected);
+    it('should call fetch with the correct parameters', () => {
+      fetchMovie(1);
+      expect(window.fetch).toHaveBeenCalledWith("https://ghibliapi.herokuapp.com/films")
     });
-  });
-
-  describe("addFavorite", () => {
-    it("calls fetch with the correct parameters when adding a new favorite", async () => {
+    it('should return a response if the status is OK', async () => {
+      const result = await fetchMovie(1);
+      expect(result).toEqual(mockMovieData);
+    });
+    it("should return an error is the status is not OK", async () => {
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
-          json: () => {
-            return Promise.resolve();
-          },
-          ok: true
-        });
+          ok: false
+        })
       });
-      const movie = {
-        title: "Avengers",
-        poster_path: "posterImage",
-        release_date: 2019,
-        vote_average: 10,
-        overview: "synopsis"
-      };
-      const movie_id = 1;
-      const user_id = 2;
-      const expected1 = "http://localhost:3000/api/users/favorites/new";
-      const expected2 = {
-        method: "POST",
-        body: JSON.stringify({
-          movie_id,
-          user_id,
-          title: movie.title,
-          poster_path: movie.poster_path,
-          release_date: movie.release_date,
-          vote_average: movie.vote_average,
-          overview: movie.overview
-        }),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      };
-      await actions.addFavorite(movie_id, user_id, movie);
-      expect(window.fetch).toHaveBeenCalledWith(expected1, expected2);
+      await expect(fetchMovie(1)).rejects.toEqual(Error('Error loading movies'))
     });
   });
-
-  describe("getFavorites", () => {
-    it("calls fetch with the correct parameters", async () => {
+  describe('fetchPeople', () => {
+    let mockPeopleData = [{name: 'San'}]
+    beforeEach(() => {
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
-          json: () => {
-            return Promise.resolve();
-          },
-          ok: true
+          ok: true,
+          json: () => Promise.resolve(mockPeopleData)
         });
       });
-      const mockUserId = 1;
-      const url = `http://localhost:3000/api/users/${mockUserId}/favorites`;
-      await actions.getFavorites(mockUserId);
-      expect(window.fetch).toHaveBeenCalledWith(url);
     });
-  });
 
-  describe("deleteFavorite", () => {
-    it("calls fetch with the correct parameters", async () => {
+    it('should call fetch with the right parameters', () => {
+      fetchPeople('people');
+      expect(window.fetch).toHaveBeenCalledWith('https://ghibliapi.herokuapp.com/people')
+    });
+
+    it('should return a response if the status is OK', async () => {
+      const result = await fetchPeople('people');
+      expect(result).toEqual(mockPeopleData);
+    });
+
+    it("should return an error is the status is not OK", async () => {
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
-          json: () => {
-            return Promise.resolve();
-          },
-          ok: true
-        });
+          ok: false
+        })
       });
-      const user_id = 1;
-      const movie_id = 2;
-      const expected1 = `http://localhost:3000/api/users/${user_id}/favorites/${movie_id}`;
-      const expected2 = {
-        method: "DELETE",
-        body: JSON.stringify({ user_id, movie_id }),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      };
-      await actions.deleteFavorite(user_id, movie_id);
-      expect(window.fetch).toHaveBeenCalledWith(expected1, expected2);
+      await expect(fetchPeople('people')).rejects.toEqual(Error('Error loading people'))
     });
   });
-});
+  describe('fetchLocation', () => {
+    let mockLocation
+    let mockPeople;
+    beforeEach(() => {
+      mockPeople = [{
+        name: 'San', 
+        location: 'https://ghibliapi.herokuapp.com/locations'
+      }];
+      mockLocation = { name: 'Irontown', climate: 'Continental' }
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockLocation)
+        });
+      });   
+    });
+
+    it('should call fetch with the correct parameters for each person object', () => {
+      fetchLocations(mockLocation);
+      expect(window.fetch).toHaveBeenCalledWith('https://ghibliapi.herokuapp.com/locations');
+    });
+
+    it("should return an error is the status is not OK", async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      await expect(fetchLocations(mockPeople)).rejects.toEqual(
+        Error("Error loading locations")
+      );
+    })
+  })
+})
