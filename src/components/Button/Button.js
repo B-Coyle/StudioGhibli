8cb031b1023d518200138501a6cf7041f2857590
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-31 08:41:32 -0600

    Add link routing to buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './Button.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./Button.css";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Button() {
   return (
-    <section className="mainBtns"  >
-      <button className="button">Films</button>
-      <button className="button">Characters</button>
-      <button className="button">Location</button>
+    <section className="mainBtns">
+      <Link to="/films">
+        <button className="button">Films</button>
+      </Link>
+      <Link to="/characters">
+        <button className="button">Characters</button>
+      </Link>
+      <Link to="/locations">
+        <button className="button">Locations</button>
+      </Link>
     </section>
   );
 }
