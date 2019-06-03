fde620c41351cb520e04b3606797e51e3584491a
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 15:31:59 -0600

    Add tests to locationsReducer and peopleReducer

---
+++ src/reducers/locationsReducer.test.js
@@ -0,0 +1,18 @@
+import {getLocationsReducer} from '../reducers/locationsReducer';
+import {getLocations} from '../actions/index';
+
+describe('getLocationsReducer', () => {
+
+    const mockLocation = [{name: "Irontown"}]
+    it('should return a default state', () => {
+        const expected = []
+        const result = getLocationsReducer(undefined ,{})
+        expect(result).toEqual(expected)
+    });
+
+    it('should return an action message' , () => {
+        const expected = 'Error fetching information'
+        const result = getLocationsReducer(undefined, getLocations('Error fetching information'))
+        expect(result).toEqual(expected);
+    })
+})
